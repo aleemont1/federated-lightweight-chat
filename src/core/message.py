adfea5247c1b9fb6088118f9094828583a4d9678
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-12-02 20:01:10 +0100

    [MESSAGE-SCHEMA] Create first immutable message structure

---
+++ src/core/message.py
@@ -0,0 +1,18 @@
+import uuid
+import time
+from typing import Dict
+from pydantic import BaseModel, Field
+
+# Map (NodeID: Counter)
+VectorClock = Dict[str, int]
+
+
+class Message(BaseModel):
+    """Message structure in the app."""
+
+    message_id: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    room_id: str
+    sender_id: str
+    content: str
+    vector_clock: VectorClock = Field(default_factory=dict)
+    created_at: float = Field(default_factory=time.time)
