1852eb2fa8d2b93bf00e7163bcaa66d9a21ffac7
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-12-03 13:25:35 +0100

    [UNIT-TESTS] Fix storage unit test linting errors

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,12 +1,14 @@
 """Unit tests for storage service and CRUD ops on DB."""
 
+# Disabling pylint warning as it is a false positive due to pytest fixtures.
+# pylint: disable=redefined-outer-name
 import pytest
 from src.services.storage import StorageService
 from src.core.message import Message
 
 
 @pytest.fixture
-def test_storage(tmp_path):
+def storage_service(tmp_path):
     """
     Creates a temporary file-based DB.
     tmp_path is a built-in pytest fixture that provides a temporary directory
@@ -15,7 +17,7 @@ def test_storage(tmp_path):
     return StorageService(str(db_file))
 
 
-def test_add_and_retreive_message(test_storage):
+def test_add_and_retreive_message(storage_service):
     """
     Tests message addition and retrieval
     """
@@ -26,10 +28,10 @@ def test_add_and_retreive_message(test_storage):
         vector_clock={"node_a": 1, "node_b": 2},
     )
 
-    test_storage.add_message(msg)
+    storage_service.add_message(msg)
 
-    assert test_storage.message_exists(msg.message_id)
-    retrieved_msg = test_storage.get_all_messages()
+    assert storage_service.message_exists(msg.message_id)
+    retrieved_msg = storage_service.get_all_messages()
 
     assert len(retrieved_msg) == 1
     loaded_msg = retrieved_msg[0]
@@ -43,22 +45,22 @@ def test_add_and_retreive_message(test_storage):
     assert loaded_msg.vector_clock["node_a"] == 1
 
 
-def test_get_latest_clock(test_storage):
+def test_get_latest_clock(storage_service):
     """
     Test latest vector clock retreival
     """
     msg1 = Message(room_id="r", sender_id="s", content="1", vector_clock={"node_a": 10})
     msg2 = Message(room_id="r", sender_id="s", content="2", vector_clock={"node_a": 25, "node_b": 10})
 
-    test_storage.add_message(msg1)
-    test_storage.add_message(msg2)
+    storage_service.add_message(msg1)
+    storage_service.add_message(msg2)
 
-    assert test_storage.get_latest_clock("node_a") == 25
-    assert test_storage.get_latest_clock("node_b") == 10
-    assert test_storage.get_latest_clock("node_c") == 0
+    assert storage_service.get_latest_clock("node_a") == 25
+    assert storage_service.get_latest_clock("node_b") == 10
+    assert storage_service.get_latest_clock("node_c") == 0
 
 
-def test_add_and_retreive_peers(test_storage):
+def test_add_and_retreive_peers(storage_service):
     """
     Test peers retreival
     """
@@ -68,16 +70,16 @@ def test_add_and_retreive_peers(test_storage):
     peer_1 = "http://peer_1:8000"
     peer_2 = "http://peer_2:8000"
 
-    test_storage.add_peer(room_a, peer_1)
-    test_storage.add_peer(room_a, peer_2)
-    test_storage.add_peer(room_b, peer_1)
+    storage_service.add_peer(room_a, peer_1)
+    storage_service.add_peer(room_a, peer_2)
+    storage_service.add_peer(room_b, peer_1)
 
-    peers_room_a = test_storage.get_peers(room_a)
+    peers_room_a = storage_service.get_peers(room_a)
     assert len(peers_room_a) == 2
     assert peer_1 in peers_room_a
     assert peer_2 in peers_room_a
 
-    peers_room_b = test_storage.get_peers(room_b)
+    peers_room_b = storage_service.get_peers(room_b)
     assert len(peers_room_b) == 1
     assert peer_1 in peers_room_b
     assert peer_2 not in peers_room_b
