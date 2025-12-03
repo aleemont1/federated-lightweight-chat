280a1ca37c3022d8533b52e7bee9fab5ede5a8a3
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-12-03 17:21:14 +0100

    [NODE_STATE] Fix linting and remove unused imports

--- src/core/node.py
+++ src/core/node.py
@@ -2,25 +2,28 @@
 
 from dataclasses import dataclass
 from typing import Dict
-from datclasses import datclass
 
-from src.core.message import Message, VectorClock
+from src.core.message import VectorClock
 from src.core.vector_clock import VectorClockService
+
 
 @dataclass
 class NodeState:
     """
     Keeps the in-memory state of the node.
     """
+
     node_id: str
     # Mapping vector clocks to each room
     room_clocks: Dict[str, VectorClock]
 
     def join_room(self, room_id: str):
+        """Joins a node to the specified room"""
         if room_id not in self.room_clocks:
             self.room_clocks[room_id] = {self.node_id: 0}
 
     def get_clock(self, room_id: str) -> VectorClock:
+        """Returns the vector clock of the node corresponding to the specified room."""
         return self.room_clocks.get(room_id, {self.node_id: 0})
 
     def update_clock(self, room_id: str, remote_clock: VectorClock) -> VectorClock:
@@ -30,7 +33,8 @@ class NodeState:
         self.room_clocks[room_id] = merged
         return merged
 
-    def increment_clock(self, room_id: str) -> VectorClock
+    def increment_clock(self, room_id: str) -> VectorClock:
+        """Increments the vector clock counter of the node in the specified room"""
         new_clock = VectorClockService.increment(self.get_clock(room_id), self.node_id)
         self.room_clocks[room_id] = new_clock
         return new_clock
